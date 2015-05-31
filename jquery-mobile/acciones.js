001f9ab6c6a8877cdce72a64787246a5481ea82c
mitzi lizbeth calva chacon <fresa_1807@hotmail.com>
2015-05-31 17:16:23 -0500

    mi pagina
    la pagina de liz

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+$(document).ready(function(e){
+$('#principal').height($('#page').height());//calcular el alto de la pantalla del dispositivo
+    document.addEventListener("deviceready",function(){
+	//precarga el sonido
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('do','audio/DO.mp3',function(){},function(e){alert('Error '+e);});	
+	audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error '+e);});		
+	audio.preloadFX('mi','audio/MI.mp3',function(){},function(e){alert('Error '+e);});		
+	audio.preloadFX('fa','audio/FA.mp3',function(){},function(e){alert('Error '+e);});		
+	audio.preloadFX('sol','audio/SOL.mp3',function(){},function(e){alert('Error '+e);});		
+	audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error '+e);});		
+	audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error '+e);});	
+	
+	//reproducir las notas
+	
+	$('.nota').bind('touchstart', function(){$(this).addClass('tocada');
+	audio.play($(this).attr('id'));});
+	$('.nota').bind('touchend',function(){$(this).removeClass('tocada');});
+	
+	},false); //deviceteady
+});//ready
